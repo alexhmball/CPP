97eb31ef939984965491f118d9d74e3d6b3dacb1
Alex <aball@student.42abudhabi.ae>
2023-03-18 10:15:30 +0400

    gotta pull

--- 02/ex01/main.cpp
+++ 02/ex01/main.cpp
@@ -6,7 +6,7 @@ Fixed a;
 Fixed const b( 10 );
 Fixed const c( 42.42f );
 Fixed const d( b );
-a = Fixed( 1234.4321f );
+a = Fixed( 2147483647.1f );
 std::cout << "a is " << a << std::endl;
 std::cout << "b is " << b << std::endl;
 std::cout << "c is " << c << std::endl;
