26daac968c6ef50a03b80d0a6bc04443ce5e58a4
Alexandra Ball <alex.ball4000@gmail.com>
2023-05-13 18:03:47 +0400

    conversions done ex00 06

--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -6,30 +6,46 @@
 #include <limits>
 #include <cctype>
 #include <cstring>
+#include <sstream>
+#include <cmath>
 
 class ScalarConverter {
 	private:
-		static char _char;
-		static int _int;
-		static float _float;
-		static double _double;
-		bool _nan;
-		bool _nanf;
-		bool _pos_inff;
-		bool _pos_inf;
-		bool _neg_inff;
-		bool _neg_inf;
-	public:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter &S);
 		~ScalarConverter();
 		ScalarConverter &operator=(const ScalarConverter &S);
-		// static bool isSpecial(std::string str);
-		static bool isNotDisplayable(char c);
-		// static char toChar(std::string str);
-		// static int toInt(std::string str);
-		// static float toFloat(std::string str);
-		// static double toDouble(std::string str);
+		static bool isSpecial(std::string str);
+		static bool isChar(std::string str);
+		static bool isFloat(std::string str);
+		static bool isInt(std::string str);
+		static bool isDouble(std::string str);
+		static char toChar(std::string str);
+		static int toInt(std::string str);
+		static float toFloat(std::string str);
+		static double toDouble(std::string str);
+		static void charToAll(char c);
+		static void intToAll(int x);
+		static void floatToAll(float f);
+		static void doubleToAll(double d);
+		static void printChar(char c);
+		static void printInt(int x);
+		static void printFloat(float f);
+		static void printDouble(double d);
+		static void convertSpecail(std::string str);
+		static void nanToAll(double d);
+		static void nanfToAll(float f);
+		static double nanToDouble(std::string str);
+		static float nanfToFloat(std::string str);
+		static double infToDouble(std::string str);
+		static void infToAll(double d);
+		static float inffToFloat(std::string str);
+		static void inffToAll(float f);
+		static double negInfToDouble(std::string str);
+		static float negInffToFloat(std::string str);
+		static void failure(void);
+	public:
+		static void convert(std::string str);
 };
 
 #endif
--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -1,18 +1,24 @@
 #include "ScalarConverter.hpp"
 
 int main( int ac, char **arg ) {
-	float x = -std::numeric_limits<float>::infinity();
-	int z = std::numeric_limits<int>::infinity();
-	std::cout << x << std::endl;
-	std::cout << z << std::endl;
 	std::string str;
+	// ScalarConverter test;
+
 	if (ac == 2)
 	{
+		// float f;
+		// std::istringstream iss ("inf");
+
+		// iss >> f;
+		// if (iss.fail())
+		// 	std::cout << "somethign wrong\n";
+		// else
+		// 	std::cout << f << std::endl;
 		str = static_cast<std::string>(arg[1]);
-		std::cout << str.find('.') << std::endl;
-		std::cout << std::numeric_limits<size_t>::max() << std::endl;
-		std::cout << ScalarConverter::isNotDisplayable(67);
-		// std::cout << ScalarConverter::isSpecial(str);
+		// float f = std::numeric_limits<float>::infinity();
+		// if (std::numeric_limits<float>::has_infinity)
+		// 	std::cout << f << std::endl;
+		ScalarConverter::convert(str);
 	}
 	else
 		std::cout << "one argument is required, not more, not less.\n";
