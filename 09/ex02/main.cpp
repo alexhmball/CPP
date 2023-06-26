338922652a40763c5d663c4e36c93065d72f569f
Alexandra Ball <alex.ball4000@gmail.com>
2023-06-26 20:16:31 +0400

    parsed sorting now to sort

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -1,61 +1,68 @@
 #include <deque>
-#include <set>
-// #include <multimap>
+#include <list>
 #include <utility>
 #include <sstream>
 #include <iostream>
 
-int main( int ac, char **av ) {
-	std::deque<size_t> test;
-	std::multiset<size_t> check;
-	std::multiset<std::pair<size_t, size_t> > sort;
-	std::multiset<size_t> sorted;
+bool check_space(std::string tmp) {
+	for (size_t i = 0; i < tmp.length(); i++) {
+		if (!isspace(tmp[i]))
+			return false;
+	}
+	return true;
+}
+
+bool check_sign(std::string tmp) {
+	for (size_t i = 0; i < tmp.length(); i++) {
+		if (!isdigit(tmp[i]))
+			return false;
+	}
+	return true;
+}
+
+bool check_sort(std::string str) {
+	std::deque<size_t> check;
+	std::string tmp;
+	std::string::size_type pos1;
+	std::string::size_type pos2;
+	std::string::size_type i = 0;
 	size_t num;
 
-	if (ac > 1) {
-		for (size_t i = 1; av[i]; i++) {
-			std::stringstream ss(av[i]);
-
-			ss >> num;
-			if (!ss.fail() && av[i][0] != '-') {
-				test.push_back(num);
-				check.insert(num);
-			} else {
-				std::cout << "Error" <<std::endl;
-				return 1;
-			}
-		}
-		while ( test.size() > 0 ) {
-			if (test.size() == 1)
-				break ;
-			if (test.back() > test.front()) {
-				sort.insert(std::pair<size_t, size_t>(test.back(), test.front()));
-			} else if (test.front() > test.back()) {
-				sort.insert(std::pair<size_t, size_t>(test.front(), test.back()));
-			}
-			test.pop_back();
-			test.pop_front();
-		}
-		for (std::set<std::pair<size_t, size_t> >::iterator it = sort.begin(); it != sort.end(); it++) {
-			if (test.size() == 1) {
-				sorted.insert(test.front());
-				test.pop_back();
-			}
-			sorted.insert((*it).second);
-			sorted.insert((*it).first);
-		}
-		// for (size_t i = 0; i < test.size(); i++) {
-		// 	std::cout << test[i] << " ";
-		// }
-		// std::cout << std::endl;
-		// for (size_t i = 0; i < sort.size(); i++) {
-		// 	std::set<std::pair<size_t, size_t> >::iterator it = check.find(test[i]);
-		// 	std::cout << (*it).first << " ";
-		// }
-		std::cout << std::endl;
-		for (std::set<size_t>::iterator it = sorted.begin(); it != sorted.end(); it++) {
-			std::cout << *it << " ";
-		}
-		std::cout << std::endl;
+	while (str[i]) {
+		while (isspace(str[i]) && str[i])
+			i++;
+		pos1 = i;
+		while (!isspace(str[i]) && str[i])
+			i++;
+		pos2 = i;
+		tmp = str.substr(pos1, pos2 - pos1);
+		std::stringstream ss(tmp);
+		ss >> num;
+		std::cout << tmp << std::endl;
+		if ((ss.fail() && !check_space(tmp)) || !check_sign(tmp))
+			return false;
+		else if (!check_space(tmp))
+			check.push_back(num);
 	}
+	for (size_t j = 0; j < check.size(); j++) {
+		std::cout << check[j] << " ";
+	}
+	std::cout << std::endl;
+	return true;
+}
+
+int main( int ac, char **av ) {
+	std::string str;
+
+	if (ac > 1) {
+		for (int i = 1; av[i]; i++) {
+			str += &av[i][0];
+			str += " ";
+		}
+		if (!check_sort(str))
+			std::cout << "Error" << std::endl;
+	} else {
+		std::cout << "Error" << std::endl;
+	}
+	return 0;
 }
